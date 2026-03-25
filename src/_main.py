6ee8e25fb58fa02ab2f50bc19827cf4c4eddb333
kaiji4ur-rgb <kaiji4ur@gmail.com>
2026-03-25 21:48:34 +0900

    20260325

---
+++ src/_main.py
@@ -0,0 +1,27 @@
+import os
+import argparse
+
+def main():
+
+    result: Optional[int] = os.system("")
+    return result
+
+
+if __name__ == "__main__":
+    # パーサーの作成
+    parser = argparse.ArgumentParser(description="プログラムの説明")
+
+    # 引数の追加
+    parser.add_argument("name", help="名前を入力してください")
+    parser.add_argument("-a", "--age", type=int, help="年齢を入力（数値）")
+
+    # 引数を解析
+    args = parser.parse_args()
+
+    print(f"こんにちは、{args.name}さん")
+    if args.age:
+        print(f"年齢は{args.age}歳です")
+    
+
+    # Run the main function
+    main()
